05f3029813422d9a3c6210de136437ee368c399b
Doaa Magdy <71094316+doaamagdy2024@users.noreply.github.com>
2022-12-03 14:45:13 +0200

    re-adding the files again with lowercase names

---
+++ cypress/support/home.js
@@ -0,0 +1,177 @@
+import HOMECOMP from './home-comp.json'
+import SETTINGSCOMP from './settings-comp.json'
+import LOGINCOMP from './login-comp.json'
+import SIGNUPCOMP from './signup-comp.json'
+
+class Home {
+  navigate () {
+    cy.visit(HOMECOMP.URL)
+  }
+
+  gotologin(){
+    cy.get(HOMECOMP.LOGIN)
+        .should('exist')
+        .click();
+    //assertion
+    cy.url().should('eq', Cypress.env('baseUrl') +  LOGINCOMP.URL);
+}
+
+  gotosignup(){
+    cy.get(HOMECOMP.SIGNUP)
+        .should('exist')
+        .click();
+    //assertion
+    cy.url().should('eq', Cypress.env('baseUrl') +  SIGNUPCOMP.URL);
+}
+
+  gotousersetting(){
+    cy.get(HOMECOMP.DROPDOWNRIGHT)
+        .should('exist')
+        .click();
+    cy.get(HOMECOMP.USERSETTINGS)
+        .should('exist')
+        .click();
+    //assertion
+    cy.url().should('eq', Cypress.env('baseUrl') +  SETTINGSCOMP.ACCOUNT);  
+  }
+
+  comebacktohome(){
+    cy.get(HOMECOMP.REDDITICON)
+        .should('exist')
+        .click();
+
+    //assertion
+    cy.url().should('eq', Cypress.env('baseUrl') +  HOMECOMP.URL);
+  }
+
+  openclosecommwindow(){
+    cy.get(HOMECOMP.DROPDOWNRIGHT)
+        .should('exist')
+        .click();
+    cy.get(HOMECOMP.CREATECOMM)
+        .should('exist')
+        .click();
+    cy.get(HOMECOMP.CLOSECOMMFORM)
+        .should('exist')
+        .click();
+    //assertion --> make sure the comm form is closed
+    cy.get(HOMECOMP.COMMFORM)
+        .should('not.exist'); 
+  }
+
+  opencancelcommwindow(){
+    cy.get(HOMECOMP.DROPDOWNRIGHT)
+        .should('exist')
+        .click();
+    cy.get(HOMECOMP.CREATECOMM)
+        .should('exist')
+        .click();
+    cy.get(HOMECOMP.CANCELCREATECOMM)
+        .should('exist')
+        .click();
+    //assertion --> make sure the comm form is closed
+    cy.get(HOMECOMP.COMMFORM)
+        .should('not.exist'); 
+  }
+
+  createcommunity(name, type, adult){   //type:  0 -->
+    cy.get(HOMECOMP.DROPDOWNRIGHT)
+        .should('exist')
+        .click();
+    cy.get(HOMECOMP.CREATECOMM)
+        .should('exist')
+        .click();
+    cy.get(HOMECOMP.COMMNAME)
+        .should('exist')
+        .clear()
+        .type(name);
+    
+    cy.get(HOMECOMP.COMMTYPE)
+        .should('exist')
+        .contains(type).click();
+    if(adult)
+        cy.get(HOMECOMP.ADULTCONTENT)
+            .should('exist')
+            .click();
+    cy.get(HOMECOMP.CREATEBUTTON)
+        .should('exist')
+        .click();
+    
+    
+  }
+  asertioncreatecommunity(name){
+    //assertions  --> 1-Form is no longer exist
+    //2-url is the community url  
+    //3-if for adult (then there will be a pop up window to ask about my age)
+    cy.get(HOMECOMP.COMMFORM)
+        .should('not.exist'); 
+    cy.url().should('eq', Cypress.env('baseUrl') +  HOMECOMP.URL + "/r/" + name + "/")
+
+  }
+
+
+  changestatus(){
+    cy.get(UserHomeComp.DROPDOWNRIGHT)
+        .should('exist')
+        .click();
+    //default --> true
+    cy.get(UserHomeComp.ONLINESTATUS)
+        .should('exist')
+        .should('have.attr', 'aria-checked', 'true')
+        .click();
+    //check that it have been changed 
+    cy.get(UserHomeComp.ONLINESTATUS)
+        .should('exist')
+        .should('have.attr', 'aria-checked', 'false')
+        .click();
+  }
+
+  createpostbyicon(){
+    
+
+  }
+
+  followuser(user){
+    //search the user in the search bar
+    cy.get(USERHOMECOMP.SEARCH)
+        .should('exist')
+        .click()
+        .clear()
+        .type(user)
+        .type('{enter}');
+
+    cy.get(USERHOMECOMP.SEARCHPEOPLE)
+        .should('exist')
+        .click();
+    
+    //can make assertion here --> to check that the url changed so that type=user
+
+    cy.get(USERHOMECOMP.FOLLOW)
+        .should('exist')
+        .click();
+
+    //make sure the button turned to following 
+    cy.get(USERHOMECOMP.FOLLOW)
+        .should('exist')
+        .contains('Following');
+
+    //and if i hover with the mouse then it will turn to unfollow
+    cy.get(USERHOMECOMP.FOLLOW)
+        .should('exist')
+        .trigger('mouseover')
+        .contains('Unfollow');
+
+    //follow him
+  }
+
+  logout(){
+    cy.get(HOMECOMP.LOGOUT)
+        .should('exist')
+        .click()
+
+    //assertion
+    cy.get(HOMECOMP.LOGIN).should('exist');
+  }
+  
+}
+export default Home
---
+++ cypress/support/login.js
@@ -0,0 +1,106 @@
+import LOGINCOMP from './login-comp.json'
+
+class Login {
+  navigate () {
+    cy.visit(LOGINCOMP.URL)
+  }
+
+  signup () {
+    cy.get(LOGINCOMP.SIGNUP).click()
+  }
+
+  forgetusername(){
+    cy.get(LOGINCOMP.PARENTFORGET)
+      .children().first()
+      .click()
+
+  }
+
+  forgetpassword(){
+    cy.get(LOGINCOMP.PARENTFORGET)
+      .children().eq(1)
+      .click();
+
+  }
+
+  // /////////////////////////////// username & password /////////////////////////
+
+  username (username) {
+    cy.get(LOGINCOMP.USERNAME)
+      .should('exist')
+      .clear()
+      .type(username)
+    return this
+  }
+
+  password (password) {
+    cy.get(LOGINCOMP.PASSWORD)
+      .clear()
+      .type(password)
+    return this
+  }
+
+  submit () {
+    cy.get(LOGINCOMP.LOGIN).click()
+  }
+
+
+  // /////////////////////////////// gmail & password /////////////////////////
+
+  //TODO --> it doesn't work
+  // withgoogle (email, password) {
+  //   cy.get(LOGINCOMP.WITHGOOGLE)
+  //     .should('exist')
+  //     .click()
+
+  //     cy.wait(5000);
+
+
+  //     cy.window().then(win => {
+  //       cy.stub(win, 'open').as('windowOpen');
+  //     });
+
+  //     cy.wait(5000);
+  //    // cy.get('#open-window').click()
+  //     // Google Login Redirection: Email Input
+  //     //cy.url().should('contain', 'accounts.google.com')
+  //       cy.get('input[type="email"]')
+  //       .clear()
+  //       .type(email)
+  //       .type('{enter}').wait(3000);
+
+  //   // Google Login Redirection: Password Input
+  //   //cy.url().should('contain', 'accounts.google.com')
+  //     cy.get('input[type="password"]')
+  //     .clear()
+  //     .type(password)
+  //     .type('{enter}').wait(3000);
+
+    
+  // }
+
+  // gglemail (gmail) {
+  //   cy.get(LOGINCOMP.EMAIL)
+  //     .should('exist')
+  //     .clear()
+  //     .type(gmail)
+  //   return this
+  // }
+
+  // gglpassword (password) {
+  //   cy.get(LOGINCOMP.PASSGGL)
+  //     .should('exist')
+  //     .clear()
+  //     .type(password)
+  //   return this
+  // }
+
+  // pressnext1 () {
+  //   cy.get(LOGINCOMP.NEXTBUTTON1)
+  // }
+
+  // pressnext2 () {
+  //   cy.get(LOGINCOMP.NEXTBUTTON2)
+  // }
+}
+export default Login
