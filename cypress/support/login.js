4e43d185b248a9eb2dfd0c22915189071556f4ed
Doaa Magdy <71094316+doaamagdy2024@users.noreply.github.com>
2022-12-03 15:53:44 +0200

    delete un-needed comments from login

--- cypress/support/login.js
+++ cypress/support/login.js
@@ -62,62 +62,6 @@ class Login {
   }
 
 
-  // /////////////////////////////// gmail & password /////////////////////////
 
-  //TODO --> it doesn't work
-  // withgoogle (email, password) {
-  //   cy.get(LOGINCOMP.WITHGOOGLE)
-  //     .should('exist')
-  //     .click()
-
-  //     cy.wait(5000);
-
-
-  //     cy.window().then(win => {
-  //       cy.stub(win, 'open').as('windowOpen');
-  //     });
-
-  //     cy.wait(5000);
-  //    // cy.get('#open-window').click()
-  //     // Google Login Redirection: Email Input
-  //     //cy.url().should('contain', 'accounts.google.com')
-  //       cy.get('input[type="email"]')
-  //       .clear()
-  //       .type(email)
-  //       .type('{enter}').wait(3000);
-
-  //   // Google Login Redirection: Password Input
-  //   //cy.url().should('contain', 'accounts.google.com')
-  //     cy.get('input[type="password"]')
-  //     .clear()
-  //     .type(password)
-  //     .type('{enter}').wait(3000);
-
-    
-  // }
-
-  // gglemail (gmail) {
-  //   cy.get(LOGINCOMP.EMAIL)
-  //     .should('exist')
-  //     .clear()
-  //     .type(gmail)
-  //   return this
-  // }
-
-  // gglpassword (password) {
-  //   cy.get(LOGINCOMP.PASSGGL)
-  //     .should('exist')
-  //     .clear()
-  //     .type(password)
-  //   return this
-  // }
-
-  // pressnext1 () {
-  //   cy.get(LOGINCOMP.NEXTBUTTON1)
-  // }
-
-  // pressnext2 () {
-  //   cy.get(LOGINCOMP.NEXTBUTTON2)
-  // }
 }
 export default Login
